7cad891b3cafa46e2d6ee06e9013e420856e3405
047pegasus <tanishqoct11@gmail.com>
2023-01-29 02:49:40 +0530

    Added MUI Components for Landing Page

--- email_verifier/src/App.js
+++ email_verifier/src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
 function App() {
   return (
     <div className="App">
@@ -9,7 +12,14 @@ function App() {
     <Typography variant="subtitle1">
     A free e-mail checking tool to check your address domains and verify its presence.
     </Typography>  
-    
+    <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+    <Button variant="outlined">Outlined</Button>
+    <Typography variant="h4">
+    Good outreach is only possible if you reach the lead’s inbox.
+    </Typography>  
+    <Typography variant="subtitle1">
+    Check every email address you have and reduce your bounce rate.
+    </Typography>  
     </div>
   );
 }
