b10e04c260e69439f848a02e2af5f80be8b34206
047pegasus <tanishqoct11@gmail.com>
2023-01-29 12:59:54 +0530

    Added skeleton to page

--- email_verifier/src/App.js
+++ email_verifier/src/App.js
@@ -6,20 +6,42 @@ import TextField from '@mui/material/TextField';
 function App() {
   return (
     <div className="App">
+    
+      <div class="head">
       <Typography variant="h2">
         XERO MAIL
-    </Typography>
+      </Typography>
+      </div>
+    
+      <div class="subs">
     <Typography variant="subtitle1">
     A free e-mail checking tool to check your address domains and verify its presence.
     </Typography>  
+    </div>
+
+    <div class="entry">
+      <div class="entryfield">
     <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+    </div>
+    <div class="entrybutton">
     <Button variant="outlined">Outlined</Button>
-    <Typography variant="h4">
-    Good outreach is only possible if you reach the lead’s inbox.
-    </Typography>  
+    </div>
+    </div>
+    
+    <div class="footer">
+      <div class="foot1">
+      <Typography variant="h4">
+      Good outreach is only possible if you reach the lead’s inbox.
+    </Typography>
+    </div>  
+    <div class="foot2">
     <Typography variant="subtitle1">
     Check every email address you have and reduce your bounce rate.
-    </Typography>  
+    </Typography>
+    </div>
+
+    </div>
+
     </div>
   );
 }
